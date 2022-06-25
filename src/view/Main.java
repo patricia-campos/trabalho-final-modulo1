f09b82a63824db26c5184221c53be56fc17f0979
Eric de Sousa Andrade <ericandrade.millenium@poupex.com.br>
2022-06-25 08:36:04 -0300

    Inicio Main

---
+++ src/view/Main.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Crie seu personagem: ");
+
+        System.out.println("Digite o nome do personagem: ");
+        String nome = sc.nextLine();
+
+
+    }
+
+
+}

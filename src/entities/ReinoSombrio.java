db7bbfbb841f93647a483292e40e65a53c582998
Eric de Sousa Andrade <ericandrade.millenium@poupex.com.br>
2022-06-24 23:26:14 -0300

    Ajustes feitos

--- src/entities/ReinoSombrio.java
+++ src/entities/ReinoSombrio.java
@@ -1,14 +1,28 @@
 package entities;
 
+import controller.Batalha;
+
+import java.util.Random;
+
 public class ReinoSombrio extends Cenario{
     private String malCheiro;
 
 
-    public void ajudaSombria(){
-
+    public void ajudaSombria(Batalha batalha){
+        Random random = new Random();
+        int sortear = random.nextInt(7);
+        if(batalha.getRoundAtual() == sortear){
+            System.out.println("A ajuda sombria te atacou enquanto você estava desatento e você perdeu " +sortear + " de vida");
+            batalha.getPersonagem().setVida(batalha.getPersonagem().getVida() - sortear);
+        }
     }
 
-    public void armadilha(){
-
+    public void armadilha(Batalha batalha){
+        Random random = new Random();
+        int sortear = random.nextInt(7);
+        if(batalha.getRoundAtual() == sortear){
+            System.out.println("Você deu um passo e não percebeu que tinha uma armadilha menos " +sortear + " de vida");
+            batalha.getPersonagem().setVida(batalha.getPersonagem().getVida() - sortear);
+        }
     }
 }

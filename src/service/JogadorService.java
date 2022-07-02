d48384147f826ce8b23bd4bccdbfea961e7eeefd
Eric de Sousa Andrade <ericandrade.millenium@poupex.com.br>
2022-07-02 19:36:59 -0300

    ajuste service

--- src/service/JogadorService.java
+++ src/service/JogadorService.java
@@ -29,6 +29,7 @@ public class JogadorService {
 
     public void remover(Jogador jogador) throws BancoDeDadosException {
         if (jogador == null) {
+
             System.out.println("Jogador não encontrado");
         } else {
             jogadorRepository.remover(jogador.getId());

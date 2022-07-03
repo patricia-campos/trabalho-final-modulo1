3de4a7f47baaefdafe9391a0fcabc54c7b4486ba
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-02 22:22:24 -0300

    Batalha Service

---
+++ src/service/BatalhaService.java
@@ -0,0 +1,48 @@
+package service;
+
+import entities.Batalha;
+import exceptions.BancoDeDadosException;
+import repository.BatalhaRepository;
+
+public class BatalhaService {
+
+    BatalhaRepository batalhaRepository = new BatalhaRepository();
+
+    public void adicionar(Batalha batalha) throws BancoDeDadosException {
+        if (batalha == null) {
+            System.out.println("Batalha não iniciada.");
+        } else {
+            batalhaRepository.adicionar(batalha);
+        }
+    }
+
+    public void listar() throws BancoDeDadosException {
+        for (Batalha batalha2 : batalhaRepository.listar()) {
+            System.out.println(batalha2);
+        }
+    }
+
+    // TODO CHECAR COMO SERÁ A REMOÇÃO, SE VAI EXISTIR REEMOÇÃO / COMO IMPLEMENTAR
+
+    public void remover(Batalha batalha) throws BancoDeDadosException {
+        if (batalha == null) {
+            System.out.println("Não existe batalha para ser removida");
+        } else {
+            batalhaRepository.remover(batalha.getIdBatalha());
+        }
+    }
+
+    // TODO CHECAR A IMPRESSÃO DO UPDATE
+
+    public void editar(Integer id, Batalha batalha) {
+
+        try {
+            boolean conseguiuEditar = batalhaRepository.editar(id, batalha);
+            System.out.println("Novo round");
+
+        } catch (BancoDeDadosException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
